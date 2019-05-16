dbef9a07d1a6cb33bffb5864abb34702eed07b41
Gabriela <xchmel27@stud.fit.vutbr.cz>
2019-05-16 17:35:49 +0300

    fixed writing into EEPROM memory

--- SetNodeId/SetNodeId.ino
+++ SetNodeId/SetNodeId.ino
@@ -1,18 +1,22 @@
 
 #include <EEPROM.h>
 
+#define EEPROM_SIZE 1
+
 // change this to be the ID of your node in the mesh network
 uint8_t nodeId = 1;
 
 void setup() {
   Serial.begin(115200);
   while (!Serial) ; // Wait for serial port to be available
+  EEPROM.begin(EEPROM_SIZE); // initialize EEPROM with predefined size
 
   Serial.println("setting nodeId...");
 
   EEPROM.write(0, nodeId);
   Serial.print(F("set nodeId = "));
   Serial.println(nodeId);
+  EEPROM.commit();
 
   uint8_t readVal = EEPROM.read(0);
 
