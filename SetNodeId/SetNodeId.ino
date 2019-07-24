bde370d62fe73d80ca899f1005c4a403e1bbfd49
Gabriela <xchmel27@stud.fit.vutbr.cz>
2019-07-24 14:35:56 +0200

    added condition for ESP32 platform

--- SetNodeId/SetNodeId.ino
+++ SetNodeId/SetNodeId.ino
@@ -1,22 +1,27 @@
 
 #include <EEPROM.h>
 
+#ifdef ESP32
 #define EEPROM_SIZE 1
-
+#endif
 // change this to be the ID of your node in the mesh network
 uint8_t nodeId = 1;
 
 void setup() {
   Serial.begin(115200);
   while (!Serial) ; // Wait for serial port to be available
+  #ifdef ESP32
   EEPROM.begin(EEPROM_SIZE); // initialize EEPROM with predefined size
+  #endif
 
   Serial.println("setting nodeId...");
 
   EEPROM.write(0, nodeId);
   Serial.print(F("set nodeId = "));
   Serial.println(nodeId);
+  #ifdef ESP32
   EEPROM.commit();
+  #endif
 
   uint8_t readVal = EEPROM.read(0);
 
